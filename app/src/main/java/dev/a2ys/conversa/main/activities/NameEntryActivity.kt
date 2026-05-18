84ed940f9a3992669a12907d55a0480f1c86f03b
zetraworld0-sketch <zetraworld0@gmail.com>
2026-05-18 13:43:26 +0100

    Create NameEntryActivity.kt

---
+++ app/src/main/java/dev/a2ys/conversa/main/activities/NameEntryActivity.kt
@@ -0,0 +1,12 @@
+package dev.a2ys.conversa.main.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.a2ys.conversa.R
+
+class NameEntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name_entry)
+    }
+}
