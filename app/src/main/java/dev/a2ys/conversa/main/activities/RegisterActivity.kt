181018945851d225e55224f8008f713a046acbff
zetraworld0-sketch <zetraworld0@gmail.com>
2026-05-18 13:41:10 +0100

    Update RegisterActivity.kt

--- app/src/main/java/dev/a2ys/conversa/main/activities/RegisterActivity.kt
+++ app/src/main/java/dev/a2ys/conversa/main/activities/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package dev.a2ys.conversa.main.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -22,8 +23,10 @@ class RegisterActivity : AppCompatActivity() {
             if (phoneNumber.length < 10) {
                 Toast.makeText(this, "Please enter a valid phone number", Toast.LENGTH_SHORT).show()
             } else {
-                // We are keeping it simple for now so it builds!
-                Toast.makeText(this, "Number saved: $phoneNumber", Toast.LENGTH_SHORT).show()
+                // Navigate to the Name Entry screen
+                val intent = Intent(this, NameEntryActivity::class.java)
+                intent.putExtra("user_phone", phoneNumber)
+                startActivity(intent)
             }
         }
     }
